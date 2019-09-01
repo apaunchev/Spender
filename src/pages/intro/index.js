060d44d455eb3789faa08cbfc253ef957c233b85
Angel Paunchev <apaunchev@gmail.com>
2019-09-01 18:12:32 +0300

    update intro

--- src/pages/intro/index.js
+++ src/pages/intro/index.js
@@ -7,11 +7,32 @@ const Intro = () => (
   <>
     <Header />
     <main className="jumbo">
-      <h1>Spender is a free and simple budgeting app.</h1>
-      <p>Your data never leaves your device and can be erased at any time.</p>
+      <h1>
+        Spender is a budgeting app with a focus on simplicity and privacy.
+      </h1>
+      <p>
+        To use it, all you need to do is set up your monthly{" "}
+        <strong>budgets</strong> for things like groceries and rent, and then
+        start tracking your daily <strong>expenses</strong>.
+      </p>
+      <p>
+        Using clean and simple visualizations, the app will then help you
+        understand how much you spend and on what, along with how much you can
+        save every month. We don’t care about your income, net worth, or credit;
+        there are better apps for tracking those.
+      </p>
+      <p>
+        We understand financial information is private and this is why we don’t
+        ask you to create an account or hand over any of your data to a third
+        party. In fact, your data never leaves your device and can be erased by
+        you at any time.
+      </p>
+      <p>
+        <strong>Ready to give it a go?</strong>
+      </p>
       <p>
         <A className="button button--primary" href="/dashboard">
-          Get started →
+          Let’s get started
         </A>
       </p>
     </main>
