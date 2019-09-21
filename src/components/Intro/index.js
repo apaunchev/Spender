1cfc3c9adba7262fb9ce1adc125e3bce00efb5b8
Angel Paunchev <apaunchev@gmail.com>
2019-09-21 17:16:31 +0300

    update intro text

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -11,20 +11,18 @@ const Intro = () => (
         <Redirect to={ROUTES.EXPENSES} />
       ) : (
         <main className="jumbo">
-          <h1>
-            Spender is a budgeting app with a focus on simplicity and privacy.
-          </h1>
+          <h1>Spender is a simple budgeting app.</h1>
           <p>
-            To use it, all you need to do is set up your monthly{" "}
-            <strong>budgets</strong> for things like groceries and rent, and
-            then start tracking your daily <strong>expenses</strong>.
+            All you need to do is set up your monthly <strong>budget</strong>{" "}
+            for things like groceries and rent, and then start tracking your{" "}
+            <strong>expenses</strong> on a daily basis.
           </p>
           <p>
-            Using clean and simple visualizations, the app will then help you
-            understand how much you spend and on what, along with how much you
-            can save every month. We don’t care about your income, net worth, or
-            credit; there are better apps for tracking those.
+            Using simple visualizations, the app will help you understand how
+            much you spend and on what, along with how much you can save every
+            month.
           </p>
+          <p>Ready to give it a try?</p>
           <SignInGoogle />
         </main>
       )
