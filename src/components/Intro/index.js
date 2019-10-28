78fe82cbba95fc47aee9cc4af23f806e46bce2ed
Angel Paunchev <apaunchev@gmail.com>
2019-10-28 22:00:55 +0200

    update intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -11,7 +11,19 @@ const Intro = () => (
         <Redirect to={ROUTES.SUBSCRIPTIONS} />
       ) : (
         <main className="jumbo">
-          <h1>Spender is a simple tracker for your recurring expenses.</h1>
+          <h1>
+            Spender helps you <mark>track your subscriptions</mark>.
+          </h1>
+          <p>
+            You need to enter each of your subscriptions (e.g. streaming
+            service, mobile carrier, broadband), along with how much it costs
+            you and how often you pay for it.
+          </p>
+          <p>
+            Spender will break down how much you spend in total per week, month,
+            or year. You decide what the limit is!
+          </p>
+          <p>Ready to give it a try?</p>
           <SignInGoogle />
         </main>
       )
