e3a5b0449cd7a226ee266f483369dbc42b35935a
Angel Paunchev <apaunchev@gmail.com>
2020-04-04 17:29:46 +0300

    upcoming: monthly chart

--- src/components/Upcoming/index.js
+++ src/components/Upcoming/index.js
@@ -40,17 +40,20 @@ class Upcoming extends React.Component {
     });
     const upcoming = new Array(12).fill([]).map((_, index) => {
       const currentMonth = addMonths(now, index).setDate(1);
+      const currentMonthSubs = subscriptions
+        .filter(s =>
+          s.remainingDueDates.find(d => isSameMonth(d, currentMonth))
+        )
+        .map(s => ({
+          ...s,
+          dueDate: s.remainingDueDates.find(d => isSameMonth(d, currentMonth))
+        }))
+        .sort((a, b) => a.dueDate - b.dueDate);
+
       return {
         id: format(currentMonth, "yyyy-MM-dd"),
-        subscriptions: subscriptions
-          .filter(s =>
-            s.remainingDueDates.find(d => isSameMonth(d, currentMonth))
-          )
-          .map(s => ({
-            ...s,
-            dueDate: s.remainingDueDates.find(d => isSameMonth(d, currentMonth))
-          }))
-          .sort((a, b) => a.dueDate - b.dueDate)
+        subscriptions: currentMonthSubs,
+        totalAmount: sumBy(currentMonthSubs, "amount")
       };
     });
 
@@ -59,6 +62,10 @@ class Upcoming extends React.Component {
 
   render() {
     const { upcoming, loading } = this.state;
+    const maxAmount = upcoming.length
+      ? Math.max(...upcoming.map(m => m.totalAmount))
+      : 0;
+    const currency = this.props.authUser.currency;
 
     if (loading) {
       return <Loading isCenter />;
@@ -69,16 +76,39 @@ class Upcoming extends React.Component {
         <header className="mb3">
           <h1 className="mb0">Upcoming</h1>
         </header>
-        {upcoming.map(({ id, subscriptions }) =>
+        <div className="chart mb3">
+          <ol className="chart__list scroll">
+            {upcoming.map(({ id, totalAmount }) => (
+              <li className="chart__item" key={id}>
+                <a
+                  href={`#${id}`}
+                  title={formatAmountInCurrency(totalAmount, currency)}
+                >
+                  <span className="chart__item__bar">
+                    <span
+                      className="chart__item__bar__segment"
+                      style={{
+                        height: `${(totalAmount / maxAmount) * 100}px`
+                      }}
+                    ></span>
+                  </span>
+                  <span className="chart__item__label">
+                    {format(parseISO(id), "MMM")}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ol>
+        </div>
+        {upcoming.map(({ id, subscriptions, totalAmount }) =>
           subscriptions.length ? (
             <div key={id}>
               <header className="flex flex--between">
-                <h2 className="mb0">{format(parseISO(id), "MMMM yyyy")}</h2>
+                <h2 id={id} className="mb0">
+                  {format(parseISO(id), "MMMM yyyy")}
+                </h2>
                 <span className="subscription-item-title">
-                  {formatAmountInCurrency(
-                    sumBy(subscriptions, "amount"),
-                    this.props.authUser.currency
-                  )}
+                  {formatAmountInCurrency(totalAmount, currency)}
                 </span>
               </header>
               <SubscriptionList
